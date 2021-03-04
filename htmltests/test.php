f9d8ba5df06fbf1fd6e59ace69e86ae31eec652d
MasterSteelblade <mastersteelblade@protonmail.com>
2021-03-04 02:46:04 +0000

    Removed redundant HTML tests folder

--- htmltests/test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once(__DIR__.'/../src/loader.php');
-
-$emoji = new Emoji();
-
-$text = "I'm a beautiful little butterfly :wf~smug: :wf~smugsan: But I need a fucking nap :miki~sleep: :pleading:";
-
-echo $emoji->parseText($text);
