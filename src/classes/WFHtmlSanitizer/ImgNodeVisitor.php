91409793edd8493fb6ae4c78a8e00e43d7e848ea
Lauren Jenkinson <lauren@kat.net.nz>
2021-05-07 15:26:58 +1200

    WFHtmlSanitizer: img: Allow `override_class` to be an array

--- src/classes/WFHtmlSanitizer/ImgNodeVisitor.php
+++ src/classes/WFHtmlSanitizer/ImgNodeVisitor.php
@@ -20,8 +20,14 @@ class ImgNodeVisitor extends AbstractNodeVisitor implements NamedNodeVisitorInte
 		$node = new ImgNode($cursor->node);
 
 		if ($this->config['override_class'] !== null) {
-			$new_classes = explode(" ", $this->config['override_class']);
-			
+			$new_classes = $this->config['override_class'];
+
+			// If our configured `override_class` is _not_ an array, then treat it as
+			// if it was a string, and explode it by the space character
+			if (!is_array($new_classes)) {
+				$new_classes = explode(" ", strval($new_classes));
+			}
+
 			if ($this->config['preserve_classes'] === true) {
 				$existing_classes = explode(" ", $this->getAttribute($domNode, 'class'));
 				$new_classes = array_unique(array_merge($new_classes, $existing_classes));
